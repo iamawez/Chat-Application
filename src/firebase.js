f5b2d0f1fc29a946daafcb3f028eb72d6c9aeee9
iamawez <awezmanyar1672@gmail.com>
2024-01-24 17:55:25 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBb1zW_XvdfKIBR7oNBD_qky5JeLJx103g",
+  authDomain: "chatapplication-f8005.firebaseapp.com",
+  projectId: "chatapplication-f8005",
+  storageBucket: "chatapplication-f8005.appspot.com",
+  messagingSenderId: "879095652120",
+  appId: "1:879095652120:web:efcae3c15cb2b0a584486d"
+};
+
+
+export const app = initializeApp(firebaseConfig);
