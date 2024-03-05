46623b80c601efd6888d597b2395186fbe9b813e
iamawez <awezmanyar1672@gmail.com>
2024-03-05 22:44:47 +0530

    update apk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import { differenceInMinutes } from 'date-fns';  // Import date-fns function
-import { AddIcon } from '@chakra-ui/icons'; 
-
+import { differenceInMinutes } from "date-fns";
+import { AddIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -9,18 +8,6 @@ import {
   HStack,
   Input,
   VStack,
-  background,
-  IconButton,
-  useStatStyles,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-
 } from "@chakra-ui/react";
 import Message from "./components/Message";
 import {
@@ -42,18 +29,15 @@ import {
   query,
   orderBy,
 } from "firebase/firestore";
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-//Login 
 const loginHandler = () => {
   const provider = new GoogleAuthProvider();
   signInWithPopup(auth, provider);
 };
 
-//Logout
 const logoutHandler = () => signOut(auth);
 
 function App() {
@@ -61,8 +45,23 @@ function App() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const divforscroll = useRef(null);
-  const fileInputRef = useRef(null);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    // Check if the user is logged in
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      setuser(data);
+
+      // If user is logged in and has not seen the alert
+      if (data && !localStorage.getItem("hasShownAlert")) {
+        alert("I am adding more features to this application. To ensure your privacy, we have implemented a feature: messages will be automatically deleted after 30 minutes.");
+        localStorage.setItem("hasShownAlert", "true");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -77,18 +76,13 @@ function App() {
         createdAt: serverTimestamp(),
       });
 
-     
-
-      // console.log(user.PhotoURL);
-
       divforscroll.current.scrollIntoView({ behavior: "smooth" });
     } catch (error) {
       alert(error);
     }
   };
 
-  
-  //Delete
+  // Delete
   const deleteHandler = async (messageId) => {
     try {
       await deleteDoc(doc(db, "Messages", messageId));
@@ -97,119 +91,38 @@ function App() {
     }
   };
 
-
-  //Image upload
-
- 
-    const [image, setImage] = useState(null);
-    const [imageUrls, setImageUrl] = useState(null);
-
-
-    const handleImageChange = (e)=>{
-      if(e.target.files[0]){
-        setImage(e.target.files[0]);
-      }
-    };
-
-
-    const handleImageUpload = async ()=>{
-      try {
-        const selectedImage = fileInputRef.current.files[0];
-        const storage = getStorage();
-        const storageRef = ref(storage, `images/${selectedImage.name || "default-name"}`);
-  
-        await uploadBytes(storageRef, selectedImage);
-  
-        const downloadURL = await getDownloadURL(storageRef);
-        setImageUrl(downloadURL);
-        // setMessage((prevMessage) => `${prevMessage}\n${downloadURL}`);
-        onClose(); // Close the modal after uploading
-      } catch (error) {
-        console.error("Error uploading image:", error);
-        alert("Error uploading image. Please try again.");
-      }
-    };
-  
-    const fetchAndDisplayImage = async (messageId) => {
-      const storage = getStorage();
-      const storageRef = ref(storage, `images/${messageId}.jpg`);
-    
-      try {
-        const downloadURL = await getDownloadURL(storageRef);
-        setImageUrl((prevImageUrls) => ({ ...prevImageUrls, [messageId]: downloadURL }));
-      } catch (error) {
-        if (error.code === 'storage/object-not-found') {
-          console.warn('Image not found. You might want to handle this case.');
-          // Set a default image URL or handle the absence of the image.
-        } else {
-          console.error("Error fetching image:", error);
-        }
-      }
-    };
-  
-    useEffect(() => {
-      // Fetch and display image when the component mounts
-      fetchAndDisplayImage();
-    }, []);
-
   useEffect(() => {
     const order = query(
       collection(db, "Messages"),
       orderBy("createdAt", "asc")
     );
 
-
     const unsubscribe = onAuthStateChanged(auth, (data) => {
       setuser(data);
     });
 
-    const unsubscribeForMessage = onSnapshot(order, (snap) => {
+    const unsubscribeForMessage = onSnapshot(order, async (snap) => {
       const currentTime = new Date();
-      setMessages(
-        snap.docs.map((item) => {
+      const updatedMessages = await Promise.all(
+        snap.docs.map(async (item) => {
           const id = item.id;
           const data = item.data();
           const messageTime = data.createdAt && data.createdAt.toDate();
-          if (messageTime && differenceInMinutes(currentTime, messageTime) >= 30) {
-            // Delete the message
+
+          if (
+            messageTime &&
+            differenceInMinutes(currentTime, messageTime) >= 30
+          ) {
             deleteHandler(id);
             return null;
-          }else{
+          } else {
             return { id, ...data };
           }
-        }).filter(Boolean)
+        })
       );
+
+      setMessages(updatedMessages.filter(Boolean));
     });
-
-
-    // const unsubscribeForMessage = onSnapshot(order, async (snap) => {
-    //   const currentTime = new Date();
-    //   const updatedMessages = await Promise.all(
-    //     snap.docs.map(async (item) => {
-    //       const id = item.id;
-    //       const data = item.data();
-    //       const messageTime = data.createdAt && data.createdAt.toDate();
-  
-    //       if (messageTime && differenceInMinutes(currentTime, messageTime) >= 30) {
-    //         // Delete the message
-    //         deleteHandler(id);
-    //         return null;
-    //       } else {
-    //         // Get download URL for imageUri
-    //         let imageUrl = null;
-    //         if (data.imageUri) {
-    //           const storage = getStorage();
-    //           const imageRef = ref(storage, data.imageUri);
-    //           imageUrl = await getDownloadURL(imageRef);
-    //         }
-  
-    //         return { id, ...data, imageUrl };
-    //       }
-    //     })
-    //   );
-  
-    //   setMessages(updatedMessages.filter(Boolean));
-    // });
 
     return () => {
       unsubscribe();
@@ -217,7 +130,6 @@ function App() {
     };
   }, []);
 
-  console.log(imageUrls)
   return (
     <Box bg={"red.50"}>
       {user ? (
@@ -238,34 +150,25 @@ function App() {
               }}
             >
               {messages.map((item) => (
-               
                 <Message
                   key={item.id}
                   user={item.uid === user.uid ? "me" : "other"}
                   text={item.text}
                   uri={item.uri}
-                  imageUrl={item?.id ? imageUrls[item.id] || null : null}               
-                      onDelete={() => deleteHandler(item.id)} // Pass deleteHandler to Message componen
+                  onDelete={() => deleteHandler(item.id)}
                 />
-               
               ))}
-               
+
               <div ref={divforscroll}></div>
             </VStack>
 
             <form onSubmit={submitHandler} style={{ width: "100%" }}>
               <HStack>
-              <IconButton
-          icon={<AddIcon />}
-          colorScheme="red"
-          style={{ width: "1px",  height:"3px", padding:"20px"  }}
-          onClick={onOpen}
-          
-        />
                 <Input
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
-                  placeholder="Enter a Message..." required
+                  placeholder="Enter a Message..."
+                  required
                 />
                 <Button colorScheme={"purple"} type="submit">
                   Send
@@ -281,26 +184,6 @@ function App() {
           </Button>
         </VStack>
       )}
-
-
-<Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Select Image</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Input type="file" ref={fileInputRef} onChange={handleImageChange} />
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button colorScheme="green" onClick={handleImageUpload}>
-              Upload
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
     </Box>
   );
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { HStack, Avatar, Text,Image,  IconButton } from "@chakra-ui/react";
-import { DeleteIcon } from '@chakra-ui/icons'; 
+import { HStack, Avatar, Text, IconButton } from "@chakra-ui/react";
+import { DeleteIcon } from '@chakra-ui/icons';
 
-const Message = ({ text, uri, user = "other", imageUrls,  onDelete }) => {
-  console.log(imageUrls)
-  // console.log("The profile ", uri)
+const Message = ({ text, uri, user = "other", onDelete }) => {
   return (
     <HStack
       alignSelf={user === "me" ? "flex-end" : "flex-start"}
@@ -15,24 +13,14 @@ const Message = ({ text, uri, user = "other", imageUrls,  onDelete }) => {
     >
       {user === "other" && <Avatar src={uri} />}
 
-      {/* <Text>{text}</Text> */}
-      {text ? (
-        <Text>{text}</Text>
-      ) : (
-        <Image src={imageUrls} alt="Uploaded Image" boxSize="150px" objectFit="cover" />
-      )}
-
-
+      {text && <Text>{text}</Text>}
 
       {user === "me" && <Avatar src={uri} />}
-
-
 
       {user === "me" && (
         <IconButton
           icon={<DeleteIcon />}
           colorScheme="red"
-          style={{ width: "2px",  height:"5px", padding:"18px" , margin:"5px" }}
           onClick={onDelete}
         />
       )}
